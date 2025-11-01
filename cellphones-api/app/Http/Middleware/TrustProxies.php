08af3ae37739257007c5ffdfca89317c6eef6b5d
HiepNguyenex <nguyenvuhiep401@gmail.com>
2025-11-02 00:05:19 +0700

    fix: sanctum csrf-cookie cross-domain config (TrustProxies + session.php)

---
+++ cellphones-api/app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Symfony\Component\HttpFoundation\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * Các proxy được tin tưởng (Render cần để gửi IP chính xác)
+     */
+    protected $proxies = '*';
+
+    /**
+     * Header dùng để xác định IP gốc của client
+     */
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO;
+}
